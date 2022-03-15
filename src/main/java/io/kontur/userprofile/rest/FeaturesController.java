e7cee598124c7df6693987f20314bc833867bb28
Ilya Semichastnov <isemichastnov@kontur.io>
2022-03-15 17:30:27 +0400

    #9001 create api for embedded applications: changed /feature paths a bit

--- src/main/java/io/kontur/userprofile/rest/FeaturesController.java
+++ src/main/java/io/kontur/userprofile/rest/FeaturesController.java
@@ -1,7 +1,6 @@
 package io.kontur.userprofile.rest;
 
 import static io.kontur.userprofile.service.FeatureService.DN2_ID;
-import static io.kontur.userprofile.service.FeatureService.DN2_NAME;
 
 import io.kontur.userprofile.model.dto.FeatureDto;
 import io.kontur.userprofile.model.entity.App;
@@ -20,19 +19,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/features")
-public class FeaturesController { //Query params are case-sensitive as per URI RFC section 6.2.2.1
+public class FeaturesController {
     private final FeatureService featureService;
     private final AppService appService;
 
     @Operation(summary = "Get default event feed for DN2 app for current user")
-    @GetMapping("/" + DN2_NAME + "/user_feed") //todo or make it use UUID instead?
+    @GetMapping("/user_feed")
     @ApiResponse(responseCode = "200",
         content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE,
             schema = @Schema(implementation = String.class)))
@@ -48,13 +47,14 @@ public class FeaturesController { //Query params are case-sensitive as per URI R
     @ApiResponse(responseCode = "200",
         content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
             array = @ArraySchema(schema = @Schema(implementation = FeatureDto.class))))
-    @GetMapping(path = "/{id}")
+    @GetMapping
     @Transactional(readOnly = true)
     public List<FeatureDto> getUserFeatures(
-        @PathVariable @Parameter(name = "id",
+        @RequestParam(name = "appId", defaultValue = "58851b50-9574-4aec-a3a6-425fa18dcb54")
+        @Parameter(name = "appId",
             example = "58851b50-9574-4aec-a3a6-425fa18dcb54") //DN2_ID, but must be constant here
-            UUID id) {
-        App app = appService.getApp(id);
+            UUID appId) {
+        App app = appService.getApp(appId);
         return featureService.getCurrentUserAppFeatures(app)
             .map(FeatureDto::fromEntity)
             .collect(Collectors.toList());
