504d9d3efe47a6e3f1be5b51ce7ff20b9199d187
avalasiuk <avalasiuk@kontur.io>
2024-01-29 18:19:38 +0100

    Make layers feature panel available for all users

---
+++ src/main/resources/changelog/sql/17517-enable-layer-feature-panel-for-all-users.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+
+--changeset user-profile-api:17517-enable-layer-feature-panel-for-all-users.sql runOnChange:false
+
+-- This feature was temporary enabled only for particular users, so make it available for everyone
+
+delete from app_user_feature 
+  where feature_id = (select f.id 
+  from feature f
+where f.name = 'layer_features_panel');
