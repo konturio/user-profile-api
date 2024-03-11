461750f5ad383600282b4d7198962df75d172246
avalasiuk <avalasiuk@kontur.io>
2024-03-11 20:23:00 +0100

    Add url

--- src/main/resources/changelog/sql/17998-add-new-parameters-in-database-for-about-pages.sql
+++ src/main/resources/changelog/sql/17998-add-new-parameters-in-database-for-about-pages.sql
@@ -7,13 +7,14 @@ create table assets(
     id            bigint not null constraint pk_assets primary key generated always as identity,
     type          text not null,
     filename      text not null,
+    url           text not null,
     description   character varying(255),
     owner_user_id bigint,
     language      text,
     last_updated  timestamp with time zone not null default current_timestamp,
-    asset         bytea not null,
     app_id        uuid not null constraint fk_assets_app references app,
-    feature_id    bigint not null constraint fk_assets_feature references feature on delete cascade
+    feature_id    bigint not null constraint fk_assets_feature references feature on delete cascade,
+    asset         bytea not null
 );
 
 alter table feature owner to "user-profile-api";
