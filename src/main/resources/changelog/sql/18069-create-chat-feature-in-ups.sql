54c9dd037abbf53224ac8a5444fa904a613c8c0a
alesiahil <112590025+alesiahil@users.noreply.github.com>
2024-04-03 01:20:04 +0300

    Update 18069-create-chat-feature-in-ups.sql

--- src/main/resources/changelog/sql/18069-create-chat-feature-in-ups.sql
+++ src/main/resources/changelog/sql/18069-create-chat-feature-in-ups.sql
@@ -12,17 +12,9 @@ select '58851b50-9574-4aec-a3a6-425fa18dcb54', id
 from feature
 where name = 'chat_panel';
 
-insert into app_user_feature (app_id, user_id, feature_id)
-select distinct app_id, user_id,  (select id from feature where name = 'chat_panel') from app_user_feature
-where app_id = '58851b50-9574-4aec-a3a6-425fa18dcb54';
-
 
 -- Kontur Atlas
 insert into app_feature (app_id, feature_id)
 select '9043acf9-2cf3-48ac-9656-a5d7c4b7593d', id
 from feature
 where name = 'chat_panel';
-
-insert into app_user_feature (app_id, user_id, feature_id)
-select distinct app_id, user_id,  (select id from feature where name = 'chat_panel') from app_user_feature
-where app_id = '9043acf9-2cf3-48ac-9656-a5d7c4b7593d';
