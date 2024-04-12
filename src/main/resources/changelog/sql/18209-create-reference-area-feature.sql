9d977ec7ad22c309b5f8c9396d9e812ca53a0140
alesiahil <112590025+alesiahil@users.noreply.github.com>
2024-04-12 18:49:37 +0300

    Update 18209-create-reference-area-feature.sql

--- src/main/resources/changelog/sql/18209-create-reference-area-feature.sql
+++ src/main/resources/changelog/sql/18209-create-reference-area-feature.sql
@@ -10,8 +10,8 @@ with cte AS
 	        (select id from feature where name = 'reference_area' limit 1) as feature_id, 
 	        null::json                                                 as configuration
 	from app
-	where app_id in ('58851b50-9574-4aec-a3a6-425fa18dcb54', '634f23f5-f898-4098-a8bd-09eb7c1e1ae5', 'c5ecc65b-1e7e-4e31-92a4-222fadeaeef0', '77260743-1da0-445b-8f56-ff6ca8520c55', '9043acf9-2cf3-48ac-9656-a5d7c4b7593d'))
-insert into app_feature (app_id, feature_id,configuration) (select * from cte) on conflict (app_id, feature_id ) do nothing;
+	where id in ('58851b50-9574-4aec-a3a6-425fa18dcb54', '634f23f5-f898-4098-a8bd-09eb7c1e1ae5', 'c5ecc65b-1e7e-4e31-92a4-222fadeaeef0', '77260743-1da0-445b-8f56-ff6ca8520c55', '9043acf9-2cf3-48ac-9656-a5d7c4b7593d'))
+insert into app_feature (app_id, feature_id, configuration) (select * from cte) on conflict (app_id, feature_id ) do nothing;
 
 insert into app_user_feature (select a.app_id, 
 	                                 a.user_id, 
