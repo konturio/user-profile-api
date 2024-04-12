0f6b6f3be2e8b710be9b222f4f0edc1f288ef7de
alesiahil <112590025+alesiahil@users.noreply.github.com>
2024-04-12 15:24:57 +0300

    Update 18209-create-reference-area-feature.sql

--- src/main/resources/changelog/sql/18209-create-reference-area-feature.sql
+++ src/main/resources/changelog/sql/18209-create-reference-area-feature.sql
@@ -5,8 +5,19 @@
 insert into feature ("name", featuretype, enabled, beta, available_for_user_apps, default_for_user_apps, description) values
     ('reference_area', 'UI_PANEL', true, false, true, false, 'Reference area layer');
 
-insert into app_feature (app_id, feature_id)
-select id, (select id from feature where name = 'reference_area') from app;
+with cte AS
+    (select id                                                         as app_id,
+	        (select id from feature where name = 'reference_area' limit 1) as feature_id, 
+	        null::json                                                 as configuration
+	from app
+	where name not in ('OpenAerialMap'))
+insert into app_feature (app_id, feature_id,configuration) (select * from cte) on conflict (app_id, feature_id ) do nothing;
 
-insert into app_user_feature (app_id, user_id, feature_id)
-select distinct app_id, user_id,  (select id from feature where name = 'reference_area') from app_user_feature;
+insert into app_user_feature (select a.app_id, 
+	                                 a.user_id, 
+	                                 (select id from feature where name = 'reference_area' limit 1) 
+	                          from (select distinct app_id, 
+	                          	                    user_id 
+	                          	    from app_user_feature 
+	                          	    where app_id in (select id from app where name not in ('OpenAerialMap'))) a)
+on conflict (app_id, user_id, feature_id) do nothing;
