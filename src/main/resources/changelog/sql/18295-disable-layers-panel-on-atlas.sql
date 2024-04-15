ae76b465338ab6036f11ebe1cf9dd270368a3b79
alesiahil <112590025+alesiahil@users.noreply.github.com>
2024-04-15 14:36:42 +0300

    Update 18295-disable-layers-panel-on-atlas.sql

--- src/main/resources/changelog/sql/18295-disable-layers-panel-on-atlas.sql
+++ src/main/resources/changelog/sql/18295-disable-layers-panel-on-atlas.sql
@@ -2,12 +2,6 @@
 
 --changeset user-profile-service:18295-disable-layers-panel-on-atlas.sql runOnChange:false
 
-delete from app_feature
-where app_id = '9043acf9-2cf3-48ac-9656-a5d7c4b7593d'
-  and feature_id in (select id
-                     from feature
-                     where name in ('map_layers_panel'));
-
 delete from app_feature (app_id, feature_id)
 select '9043acf9-2cf3-48ac-9656-a5d7c4b7593d', id
 from feature
