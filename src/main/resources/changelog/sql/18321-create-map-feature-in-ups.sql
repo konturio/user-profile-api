5f71e991d1d5eacf7ce187fe13ddc6de0e707945
alesiahil <112590025+alesiahil@users.noreply.github.com>
2024-04-18 18:27:45 +0300

    Update 18321-create-map-feature-in-ups.sql

--- src/main/resources/changelog/sql/18321-create-map-feature-in-ups.sql
+++ src/main/resources/changelog/sql/18321-create-map-feature-in-ups.sql
@@ -3,7 +3,7 @@
 --changeset user-profile-api:18321-create-map-feature-in-ups.sql runOnChange:false
 
 insert into feature ("name", featuretype, enabled, beta, available_for_user_apps, default_for_user_apps, description) 
-values ('map', 'MAP', true, false, true, true, 'Map mode which can be switched off for not logged in users');
+values ('map', 'UI_PANEL', true, false, true, true, 'Map mode which can be switched off for not logged in users');
 
 insert into app_feature (app_id, feature_id)
 select id, (select id from feature where name = 'map') from app;
