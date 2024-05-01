f1d79952998d7f073e2eaf2bf9c62468f37a763f
alesiahil <112590025+alesiahil@users.noreply.github.com>
2024-05-01 18:11:54 +0300

    Update 18417-create-llm-analytics-feature-in-ups.sql

--- src/main/resources/changelog/sql/18417-create-llm-analytics-feature-in-ups.sql
+++ src/main/resources/changelog/sql/18417-create-llm-analytics-feature-in-ups.sql
@@ -3,7 +3,7 @@
 --changeset user-profile-api:18417-create-llm-analytics-feature-in-ups.sql runOnChange:false
 
 insert into feature ("name", featuretype, enabled, beta, available_for_user_apps, default_for_user_apps, description)
-values ('llm_analytics', 'UI_PANEL', true, true, false, true, 'AI analytics panel connected to open.ai and allowed to get personalized analytics based on user`s bio from their profile');
+values ('llm_analytics', 'UI_PANEL', true, true, false, false, 'AI analytics panel connected to open.ai and allowed to get personalized analytics based on user`s bio from their profile');
 
 insert into app_feature (app_id, feature_id)
 select '9043acf9-2cf3-48ac-9656-a5d7c4b7593d', id
