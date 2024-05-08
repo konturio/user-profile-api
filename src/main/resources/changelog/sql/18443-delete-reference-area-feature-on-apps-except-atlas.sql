75434b6c5634b7efb3eed711c72e204dcb7f1ae1
alesiahil <112590025+alesiahil@users.noreply.github.com>
2024-05-08 15:04:33 +0300

    Update 18443-delete-reference-area-feature-on-apps-except-atlas.sql

--- src/main/resources/changelog/sql/18443-delete-reference-area-feature-on-apps-except-atlas.sql
+++ src/main/resources/changelog/sql/18443-delete-reference-area-feature-on-apps-except-atlas.sql
@@ -5,6 +5,4 @@
 delete
 from app_feature
 where app_id in ('58851b50-9574-4aec-a3a6-425fa18dcb54', '634f23f5-f898-4098-a8bd-09eb7c1e1ae5', 'c5ecc65b-1e7e-4e31-92a4-222fadeaeef0', '77260743-1da0-445b-8f56-ff6ca8520c55')
-  and feature_id in (select id
-                     from feature
-                     where name = 'reference_area');
+  and feature_id = 'reference_area');
