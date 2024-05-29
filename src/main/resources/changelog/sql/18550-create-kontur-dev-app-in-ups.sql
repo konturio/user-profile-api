420431ceaf72d03d9c1890436e146085481bcd0a
alesiahil <112590025+alesiahil@users.noreply.github.com>
2024-05-29 22:27:56 +0300

    Update 18550-create-kontur-dev-app-in-ups.sql

--- src/main/resources/changelog/sql/18550-create-kontur-dev-app-in-ups.sql
+++ src/main/resources/changelog/sql/18550-create-kontur-dev-app-in-ups.sql
@@ -56,4 +56,4 @@ set configuration = '{"statistics": [{
   "y": "highway_length"
 }]}'
 where app_id = '0b5b4047-3d9b-4ec4-993f-acf9c7315536'
-  and f.name = (select id from feature where name = 'analytics_panel');
+  and f.name = 'analytics_panel');
