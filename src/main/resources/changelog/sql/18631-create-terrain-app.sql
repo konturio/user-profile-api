2af5730946de7431a6711bb153328aad30b82ba7
alesiahil <112590025+alesiahil@users.noreply.github.com>
2024-05-30 01:10:27 +0300

    Update 18631-create-terrain-app.sql

--- src/main/resources/changelog/sql/18631-create-terrain-app.sql
+++ src/main/resources/changelog/sql/18631-create-terrain-app.sql
@@ -36,15 +36,14 @@ from feature f
 where f.name in ('toolbar', 'locate_me', 'map_ruler', 'osm_edit_link', 'boundary_selector', 'geometry_uploader',
                  'focused_geometry_editor', 'side_bar', 'map', 'reports', 'app_login', 'about_page', 'analytics_panel',
                  'events_list', 'map_layers_panel', 'legend_panel', 'episodes_timeline',
-                 'chat_panel', 'events_list__bbox_filter', 'current_event', 'focused_geometry_layer', 'layers_in_area',
+                 'chat_panel', 'events_list__bbox_filter', 'focused_geometry_layer', 'layers_in_area',
                  'use_3rdparty_analytics', 'tooltip', 'toasts', 'kontur-public');
 
 -- Terrain app role based features
 insert into custom_app_feature (app_id, feature_id, authenticated, role_id)
 select '3a433e95-0449-48a3-b4ff-9cffea805c74', f.id, true, r.id
 from feature f, custom_role r
-where f.name in ('mcda', 'bivariate_manager', 'kontur-public', 'llm_analytics', 'advanced_analytics_panel',                 
-                'feed_selector', 'reference_area')
+where f.name in ('mcda', 'llm_analytics', 'advanced_analytics_panel', 'feed_selector', 'reference_area')
   and r.name = 'terrain_admin';
 
 --configurate analytics panel 
