247b1de241bfeabd1832c2f14dffff2c346a8b36
alesiahil <112590025+alesiahil@users.noreply.github.com>
2024-05-29 22:17:57 +0300

    Update 18631-create-terrain-app.sql

--- src/main/resources/changelog/sql/18631-create-terrain-app.sql
+++ src/main/resources/changelog/sql/18631-create-terrain-app.sql
@@ -2,6 +2,7 @@
 
 --changeset user-profile-service:18631-create-terrain-app.sql runOnChange:false
 
+--create Terrain app
 insert into app (id, name, description, owner_user_id, is_public, extent, favicon_pack)
 values ('3a433e95-0449-48a3-b4ff-9cffea805c74', 'Terrain', 'Whitelable Finance application as a sales tool for Nick Provenzano', null, true, '{-135,0,63,62}', '{
   "favicon.svg": "/active/api/apps/3a433e95-0449-48a3-b4ff-9cffea805c74/assets/favicon.svg",
@@ -11,64 +12,40 @@ values ('3a433e95-0449-48a3-b4ff-9cffea805c74', 'Terrain', 'Whitelable Finance a
   "icon-512x512.png": "/active/api/apps/3a433e95-0449-48a3-b4ff-9cffea805c74/assets/icon-512x512.png"
 }'::json);
 
-insert into app_feature (app_id, feature_id)
-select '3a433e95-0449-48a3-b4ff-9cffea805c74', f.id
+-- create new app role
+insert into custom_role (name)
+values ('terrain_admin', 'terrain_demo');
+
+-- assign new app role to Kontur employees
+insert into user_custom_role (user_id, role_id, started_at)
+select u.id, r.id, now()
+from users u, custom_role r
+where r.name = 'terrain_admin'
+    and u.email in ('pkrukovich@kontur.io', 'ahil@kontur.io', 'avalasiuk@kontur.io', 'darafei@kontur.io',
+                    'nlaptsik@kontur.io', 'atarakanov@kontur.io', 'a.artyukevich@kontur.io', 'vbondar@kontur.io',
+                    'arben@kontur.io', 'tgrigoryan@kontur.io', 'aklopau@kontur.io', 'atsiatserkina@kontur.io',
+                    'ekarpach@kontur.io', 'kbakhanko@kontur.io', 'abaranau@kontur.io', 'curtis@kontur.io',
+                    'milvari@kontur.io', 'vkozel@kontur.io', 'amurashka@kontur.io', 'hoa@kontur.io',
+                    'gdowling@kontur.io', 'nprovenzano@kontur.io', 'tad@kontur.io', 'nharshunova@kontur.io',
+                    'achichigin@kontur.io', 'hevans@kontur.io');
+
+-- Terrain app guest features
+insert into custom_app_feature (app_id, feature_id, authenticated)
+select '3a433e95-0449-48a3-b4ff-9cffea805c74', f.id, false
 from feature f
-where f.name in ('toolbar', 'locate_me', 'map_ruler', 'osm_edit_link', 'mcda', 'boundary_selector', 'kontur-public',
-                'geometry_uploader', 'focused_geometry_editor', 'side_bar', 'map', 'reports', 'app_login', 
-                'about_page', 'analytics_panel', 'llm_analytics', 'advanced_analytics_panel', 'events_list', 'map_layers_panel', 
-                'legend_panel', 'episodes_timeline', 'chat_panel', 'feed_selector', 'events_list__bbox_filter', 
-                'focused_geometry_layer', 'reference_area', 'layers_in_area', 'use_3rd_party_analytics', 'tooltip', 'toasts');
+where f.name in ('toolbar', 'locate_me', 'map_ruler', 'osm_edit_link', 'boundary_selector', 'geometry_uploader',
+                 'focused_geometry_editor', 'side_bar', 'map', 'reports', 'app_login', 'about_page', 'analytics_panel',
+                 'events_list', 'map_layers_panel', 'legend_panel', 'episodes_timeline',
+                 'chat_panel', 'events_list__bbox_filter', 'current_event', 'focused_geometry_layer', 'layers_in_area',
+                 'use_3rdparty_analytics', 'tooltip', 'toasts', 'kontur-public');
 
-
--- turn on mcda and other necessary features for the selected users
-delete from app_user_feature where app_id = '3a433e95-0449-48a3-b4ff-9cffea805c74';
-
-with users_ids as (select id 
-                  from users 
-                  where email in ('atsiatserkina@kontur.io', 
-                                  'abaranau@kontur.io', 
-                                  'amurashka@kontur.io', 
-                                  'a.artyukevich@kontur.io', 
-                                  'avalasiuk@kontur.io', 
-                                  'darafei@kontur.io', 
-                                  'ekarpach@kontur.io', 
-                                  'pkrukovich@kontur.io', 
-                                  'tgrigoryan@kontur.io', 
-                                  'vkozel@kontur.io', 
-                                  'nlaptsik@kontur.io',                                  
-                                  'ahil@kontur.io',
-                                  'atarakanov@kontur.io',
-                                  'achichigin@kontur.io',
-                                  'nharshunova@kontur.io',
-                                  'lgudyma@kontur.io',
-                                  'me@akolesen.pro',
-                                  'hevans@kontur.io',
-                                  'gdowling@kontur.io',
-                                  'arben@kontur.io',
-                                  'curtis@kontur.io',
-                                  'hoa@kontur.io',
-                                  'nprovenzano@kontur.io',
-                                  'tad@kontur.io',
-                                  'aklopau@kontur.io',
-                                  'kbakhanko@kontur.io',
-                                  'milvari@kontur.io',
-                                  'vbondar@kontur.io',
-                                  '',
-                                  '')),
-features_ids as (select id 
-                      from feature f
-                      where f.name in ('toolbar', 'locate_me', 'map_ruler', 'osm_edit_link', 'mcda', 'boundary_selector', 'kontur-public',
-                                      'geometry_uploader', 'focused_geometry_editor', 'side_bar', 'map', 'reports', 'app_login', 
-                                      'about_page', 'analytics_panel', 'llm_analytics', 'advanced_analytics_panel', 'events_list', 'map_layers_panel', 
-                                      'legend_panel', 'episodes_timeline', 'chat_panel', 'feed_selector', 'events_list__bbox_filter', 
-                                      'focused_geometry_layer', 'reference_area', 'layers_in_area', 'use_3rd_party_analytics', 'tooltip', 'toasts'))
-insert into app_user_feature
-  select '3a433e95-0449-48a3-b4ff-9cffea805c74' as app_id,
-         users_ids.id                           as user_id,
-         features_ids.id                        as feature_id
-  from users_ids, features_ids;
-
+-- Terrain app role based features
+insert into custom_app_feature (app_id, feature_id, authenticated, role_id)
+select '3a433e95-0449-48a3-b4ff-9cffea805c74', f.id, true, r.id
+from feature f, custom_role r
+where f.name in ('mcda', 'bivariate_manager', 'kontur-public', 'llm_analytics', 'advanced_analytics_panel',                 
+                'feed_selector', 'reference_area')
+  and r.name = 'terrain_admin';
 
 --configurate analytics panel 
 update app_feature
