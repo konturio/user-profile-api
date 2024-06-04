779cbd4dab1e705e2362d8b6de6c537eab6c094b
alesiahil <112590025+alesiahil@users.noreply.github.com>
2024-06-04 12:09:10 +0300

    Update 18631-create-terrain-app.sql

--- src/main/resources/changelog/sql/18631-create-terrain-app.sql
+++ src/main/resources/changelog/sql/18631-create-terrain-app.sql
@@ -56,4 +56,5 @@ set configuration = '{"statistics": [{
   "x": "populated_area_km2"
 }]}'
 where app_id = '3a433e95-0449-48a3-b4ff-9cffea805c74'
-  and feature_id in (select f.id from feature f where f.name = 'analytics_panel');
+  and feature_id in (select f.id from feature f where f.name = 'analytics_panel')
+  and not authenticated;
