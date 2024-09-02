52c3b8a4eab45df101d64380607ab7f1355e7a86
alesiahil <112590025+alesiahil@users.noreply.github.com>
2024-09-02 21:13:23 +0300

    Update 19393-upload-odin-icons-on-github-and-ups.sql

--- src/main/resources/changelog/sql/19393-upload-odin-icons-on-github-and-ups.sql
+++ src/main/resources/changelog/sql/19393-upload-odin-icons-on-github-and-ups.sql
@@ -1,1 +1,8 @@
+--liquibase formatted sql
 
+--changeset user-profile-api:19393-upload-odin-icons-on-github-and-ups.sql runOnChange:false
+
+update app
+set sidebar_icon_url = '/active/api/apps/415e2172-3e94-4749-b714-d37470acf88a/assets/favicon.svg',
+  favicon_url = '/active/api/apps/415e2172-3e94-4749-b714-d37470acf88a/assets/favicon.ico'
+where id ='415e2172-3e94-4749-b714-d37470acf88a'
