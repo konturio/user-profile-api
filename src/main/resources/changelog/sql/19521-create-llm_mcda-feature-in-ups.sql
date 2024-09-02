4604bbe3773f3dc074941807cb61f3c5b5ccba08
alesiahil <112590025+alesiahil@users.noreply.github.com>
2024-09-02 21:36:49 +0300

    Update 19521-create-llm_mcda-feature-in-ups.sql

--- src/main/resources/changelog/sql/19521-create-llm_mcda-feature-in-ups.sql
+++ src/main/resources/changelog/sql/19521-create-llm_mcda-feature-in-ups.sql
@@ -4,20 +4,21 @@
 
 --create Search bar feature
 insert into feature ("name", featuretype, enabled, beta, available_for_user_apps, default_for_user_apps, description)
-values ('llm_mcda', 'UI_PANEL', true, false, true, false, 'LLM MCDA allows to type questions or requests in the search bar and get MCDA composed by ChatGPT based on the input and the axes Insights DB has.')
+values ('llm_mcda', 'UI_PANEL', true, true, true, false, 'LLM MCDA allows to type questions or requests in the search bar and get MCDA composed by ChatGPT based on the input and the axes Insights DB has.')
 on conflict do nothing;
 
 -- Terrain
-insert into custom_app_feature (app_id, feature_id, authenticated)
+insert into custom_app_feature (app_id, feature_id, authenticated, role_id)
 select '3a433e95-0449-48a3-b4ff-9cffea805c74', f.id, false
-from feature f
+from feature f, custom_role r
 where f.name in ('llm_mcda')
+    and r.name in ('terrain_admin')
 on conflict do nothing;
 
 -- MCDA
-insert into custom_app_feature (app_id, feature_id, authenticated)
+insert into custom_app_feature (app_id, feature_id, authenticated, role_id)
 select '77260743-1da0-445b-8f56-ff6ca8520c55', f.id, false
-from feature f
+from feature f, custom_role r
 where f.name in ('llm_mcda')
   and r.name in ('mcda_demo', 'mcda_admin')
 on conflict do nothing;
