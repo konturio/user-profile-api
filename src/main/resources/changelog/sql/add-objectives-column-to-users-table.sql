f957599210b54ff739d3c35326bac6b4d4b39891
Palina Krukovich <36711714+palina-krukovich@users.noreply.github.com>
2025-01-29 23:17:43 +0100

    Update add-objectives-column-to-users-table.sql

--- src/main/resources/changelog/sql/add-objectives-column-to-users-table.sql
+++ src/main/resources/changelog/sql/add-objectives-column-to-users-table.sql
@@ -2,6 +2,8 @@
 
 --changeset user-profile-api:add-objectives-column-to-users-table.sql runOnChange:true
 
+
+-- The existing column "bio" is converted to "objectives" along with all the contents, and a new empty "bio" column is created 
 alter table users
 rename column bio to objectives;
 
