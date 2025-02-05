4c0a39f91b7f262963edcf6ae945857d760506be
alesiahil <112590025+alesiahil@users.noreply.github.com>
2025-02-05 23:56:19 +0300

    Update 20712-set-config-in-ups-with-hidden-profile-fields-for-dn-oam-and-gg-apps.sql

--- src/main/resources/changelog/sql/20712-set-config-in-ups-with-hidden-profile-fields-for-dn-oam-and-gg-apps.sql
+++ src/main/resources/changelog/sql/20712-set-config-in-ups-with-hidden-profile-fields-for-dn-oam-and-gg-apps.sql
@@ -10,7 +10,7 @@ on conflict do nothing;
 delete from custom_app_feature where f.name in ('phone_number', 'linkedin', 'organization', 'position', 'gis_specialists')
 on conflict do nothing;
 
---set config on Disaster Ninja
+--set config for profile on Disaster Ninja
 update custom_app_feature
 set configuration = '{
             "name": "app_profile",
@@ -28,7 +28,7 @@ where app_id = '58851b50-9574-4aec-a3a6-425fa18dcb54'
   and feature_id in (select f.id from feature f where f.name = 'app_login')
   and not authenticated;
 
---set config on OpenAerialMap
+--set config for profile on OpenAerialMap
 update custom_app_feature
 set configuration = '{
             "name": "app_profile",
@@ -46,7 +46,7 @@ where app_id = '1dc6fe68-8802-4672-868d-7f17943bf1c8'
   and feature_id in (select f.id from feature f where f.name = 'app_login')
   and not authenticated;
 
---set config on Crisis Monitoring for GlobalGiving
+--set config for profile on Crisis Monitoring for GlobalGiving
 update custom_app_feature
 set configuration = '{
             "name": "app_profile",
