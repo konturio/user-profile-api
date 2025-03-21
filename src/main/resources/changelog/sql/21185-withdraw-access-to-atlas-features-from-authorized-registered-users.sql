4d8bf16c6a78d6f1bc238dc1e89c19f4ec7a5d5b
alesiahil <112590025+alesiahil@users.noreply.github.com>
2025-03-21 13:00:18 +0300

    Create 21185-withdraw-access-to-atlas-features-from-authorized-registered-users.sql

---
+++ src/main/resources/changelog/sql/21185-withdraw-access-to-atlas-features-from-authorized-registered-users.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+
+--changeset user-profile-api:21185-withdraw-access-to-atlas-features-from-authorized-registered-users.sql runOnChange:false
+
+delete from custom_app_feature 
+where app_id = '9043acf9-2cf3-48ac-9656-a5d7c4b7593d' 
+and authenticated = true
+and feature_id in (select id from feature where name in ('osm_edit_link', 'focused_geometry_layer', 'map_ruler', 'boundary_selector', 'geometry_uploader', 'legend_panel', 'layers_in_area', 
+'focused_geometry_editor', 'locate_me', 'mcda', 'toolbar', 'chat_panel', 'map', 'reference_area', 'llm_analytics', 'search_locations', 'search_bar', 
+'admin_boundary_breadcrumbs', 'llm_mcda'));
