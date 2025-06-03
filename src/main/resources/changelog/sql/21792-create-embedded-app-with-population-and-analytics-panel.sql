1dd1432565fcb5e11215bdb80e79eb9bdcd253b7
alesiahil <112590025+alesiahil@users.noreply.github.com>
2025-06-03 15:06:57 +0300

    Update 21792-create-embedded-app-with-population-and-analytics-panel.sql

--- src/main/resources/changelog/sql/21792-create-embedded-app-with-population-and-analytics-panel.sql
+++ src/main/resources/changelog/sql/21792-create-embedded-app-with-population-and-analytics-panel.sql
@@ -4,13 +4,15 @@
 
 --create embedded app
 insert into app (id, name, description, owner_user_id, is_public, extent)
-values ('b8e7eb02-ca92-4bad-affa-0c5afdb5d8b8', 'Population embedded with analytics', 'Embedded application with population and Analytics panel', null, true, '{-180,-80,180,80}');
+values ('b8e7eb02-ca92-4bad-affa-0c5afdb5d8b8', 'Population embedded with analytics', 'Embedded application with population and Analytics panel', null, true, '{-180,-80,180,80}')
+  on conflict do nothing;
 
 --add guest features
 insert into custom_app_feature (app_id, feature_id, authenticated)
 select 'b8e7eb02-ca92-4bad-affa-0c5afdb5d8b8', f.id, false
 from feature f
-where f.name in ('map', 'toolbar', 'analytics_panel', 'boundary_selector', 'focused_geometry_layer');
+where f.name in ('map', 'toolbar', 'analytics_panel', 'boundary_selector', 'focused_geometry_layer')
+  on conflict do nothing;
 
 --configurate analytics panel 
 update custom_app_feature
@@ -18,9 +20,8 @@ set configuration = '{"statistics": [{
   "formula": "sumX",
   "x": "population"
 }, {
-  "formula": "avgX",
-  "x": "population",
-  "y": "area"
+  "formula": "sumX",
+  "x": "area_km2"
 }]}'
 where app_id = 'b8e7eb02-ca92-4bad-affa-0c5afdb5d8b8'
   and feature_id in (select f.id from feature f where f.name = 'analytics_panel')
