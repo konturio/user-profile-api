0921c5d225651f4a383fed4d754f244d8dcf5744
alesiahil <112590025+alesiahil@users.noreply.github.com>
2025-07-02 16:41:32 +0300

    Update 21414-create-risk-assessment-app.sql

--- src/main/resources/changelog/sql/21414-create-risk-assessment-app.sql
+++ src/main/resources/changelog/sql/21414-create-risk-assessment-app.sql
@@ -4,7 +4,7 @@
 
 --create Risk Assessment app
 insert into app (id, name, description, owner_user_id, is_public, sidebar_icon_url, favicon_url, extent, domains, favicon_pack)
-values ('2d5af407-9f47-4f03-9d9b-2320ce9d307b', 'Hazard Compass', 'Application for risk assessment, mostly for insurance companies', null, true, 
+values ('2d5af407-9f47-4f03-9d9b-2320ce9d307b', 'Risk Compass', 'Application for risk assessment, mostly for insurance companies', null, true, 
   '/active/api/apps/2d5af407-9f47-4f03-9d9b-2320ce9d307b/assets/favicon.svg', '/active/api/apps/2d5af407-9f47-4f03-9d9b-2320ce9d307b/assets/favicon.ico', 
   '{-135,0,63,62}', array['dev-risks-kontur-io.k8s-01.konturlabs.com', 'test-risks-kontur-io.k8s-01.konturlabs.com', 'prod-risks-kontur-io.k8s-01.konturlabs.com', 'risks.kontur.io'], '{
   "favicon.svg": "/active/api/apps/2d5af407-9f47-4f03-9d9b-2320ce9d307b/assets/favicon.svg",
@@ -17,7 +17,7 @@ on conflict do nothing;
 
 --create new app role
 insert into custom_role (name)
-values ('hazard_compass_admin')
+values ('risk_compass_admin')
 on conflict do nothing;
 
 --add guest features
@@ -37,7 +37,7 @@ where f.name in ('toolbar', 'locate_me', 'map_ruler', 'osm_edit_link', 'mcda', '
                 'events_list', 'map_layers_panel', 'legend_panel', 
                 'episodes_timeline', 'feed_selector', 'events_list__bbox_filter', 'current_event',
                 'focused_geometry_layer', 'layers_in_area')
-  and r.name = 'hazard_compass_admin'
+  and r.name = 'risk_compass_admin'
   on conflict do nothing;
 
 --configurate analytics panel 
@@ -49,11 +49,17 @@ set configuration = '{"statistics": [{
   "formula": "sumX",
   "x": "area_km2"
 }, {
-  "formula": "sumX",
-  "x": "building_count"
-}, {
   "formula": "avgX",
   "x": "ghs_avg_building_height"
+}, {
+  "formula": "sumX",
+  "x": "highway_length"
+}, {
+  "formula": "sumX",
+  "x": "railway_length"
+}, {
+  "formula": "sumX",
+  "x": "osm_airports_count"
 }, {
   "formula": "maxX",
   "x": "hazardous_days_count"
@@ -62,43 +68,10 @@ set configuration = '{"statistics": [{
   "x": "cyclone_days_count"
 }, {
   "formula": "maxX",
-  "x": "drought_days_count"
-}, {
-  "formula": "maxX",
-  "x": "earthquake_days_count"
-}, {
-  "formula": "maxX",
   "x": "flood_days_count"
 }, {
   "formula": "maxX",
-  "x": "volcano_days_count"
-}, {
-  "formula": "maxX",
   "x": "wildfire_days_count"
-}, {
-  "formula": "sumX",
-  "x": "permanent_water"
-}, {
-  "formula": "avgX",
-  "x": "avg_elevation_gebco_2022"
-}, {
-  "formula": "avgX",
-  "x": "avg_forest_canopy_height"
-}, {
-  "formula": "avgX",
-  "x": "days_maxtemp_over_32c_1c"
-}, {
-  "formula": "avgX",
-  "x": "days_mintemp_above_25c_1c"
-}, {
-  "formula": "sumX",
-  "x": "osm_airports_count"
-}, {
-  "formula": "sumX",
-  "x": "osm_defibrillators_count"
-}, {
-  "formula": "sumX",
-  "x": "landmarks_and_outdoors_fsq_count"
 }]}'  
 where app_id = '2d5af407-9f47-4f03-9d9b-2320ce9d307b'
   and feature_id in (select f.id from feature f where f.name = 'analytics_panel')
