d0e2fb5df1a7d2e4f7f532629cadb750ff28dcac
alesiahil <112590025+alesiahil@users.noreply.github.com>
2025-07-09 19:09:01 +0300

    Update add-risk-compass-trial-role-to-users.sql

--- src/main/resources/changelog/sql/add-risk-compass-trial-role-to-users.sql
+++ src/main/resources/changelog/sql/add-risk-compass-trial-role-to-users.sql
@@ -7,18 +7,22 @@ INSERT INTO custom_role (name)
 VALUES ('risk_compass_trial')
 ON CONFLICT DO NOTHING;
 
--- Assign the role to users who don't have any roles
-WITH users_with_no_roles AS (
+-- Assign the role to users who don't have any trial role in Risk Assessment app
+WITH users_without_role AS (
     SELECT u.id AS user_id
     FROM users u
     WHERE NOT EXISTS (
-        SELECT 1 FROM user_custom_role ur WHERE ur.user_id = u.id
+        SELECT 1
+        FROM user_custom_role ur
+        JOIN custom_role cr ON cr.id = ur.role_id
+        WHERE ur.user_id = u.id
+          AND cr.name = 'risk_compass_trial'
     )
 )
 INSERT INTO user_custom_role (user_id, role_id, started_at, ended_at)
 SELECT
-    uwnr.user_id,
+    u.user_id,
     (SELECT id FROM custom_role WHERE name = 'risk_compass_trial' LIMIT 1),
     NOW(),
     NOW() + INTERVAL '14 days'
-FROM users_with_no_roles uwnr;
+FROM users_without_role u;
