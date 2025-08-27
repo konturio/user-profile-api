0f4077327c37f1fb32b27a963ecbe0abc3a14efe
Alex Chichigin <sad.ronin@gmail.com>
2025-08-27 18:18:03 +0400

    fix: SQL syntax in the crate initial prod users migration

--- src/main/resources/changelog/sql/create-initial-prod-users.sql
+++ src/main/resources/changelog/sql/create-initial-prod-users.sql
@@ -3,25 +3,8 @@ insert into users
 values
     (nextval('user_sequence'), 'akucharau@kontur.io', 'akucharau@kontur.io', 'Andrei', 'Kucharau', null, true, false),
     (nextval('user_sequence'), 'azapasnik', 'azapasnik@kontur.io', 'Alexander', 'Zapasnik', null, true, false),
-    
-    
     (nextval('user_sequence'), 'egrudinskaya', 'egrudinskaya@kontur.io', 'Ekaterina', 'Grudinskaya', null, true, false),
     (nextval('user_sequence'), 'eokulik@kontur.io', 'eokulik@kontur.io', 'Eugene', 'Okulik', null, true, false),
-    
-    
-    
-    
     (nextval('user_sequence'), 'isemichastnov@kontur.io', 'isemichastnov@kontur.io', 'Ilya', 'Semichastnov', null, true, false),
-    
-    
-    
-    
-    
     (nextval('user_sequence'), 'pkrukovich', 'pkrukovich@kontur.io', 'Polina', 'Krukovich', null, true, false),
-    
-    (nextval('user_sequence'), 'prytvinsky', 'prytvinsky@kontur.io', 'Pavel', 'Rytvinsky', null, true, false),
-    
-    
-    
-    
-    ;
+    (nextval('user_sequence'), 'prytvinsky', 'prytvinsky@kontur.io', 'Pavel', 'Rytvinsky', null, true, false);
